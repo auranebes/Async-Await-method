4ffed4180868148d96b27385b2ad5f5fa4926ff1
Arslan Abdullaev <ars352@yahoo.com>
2022-02-13 16:30:56 +0300

    Initial Commit

---
+++ await Method/await_MethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  await_MethodApp.swift
+//  await Method
+//
+//  Created by Arslan Abdullaev on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct await_MethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
